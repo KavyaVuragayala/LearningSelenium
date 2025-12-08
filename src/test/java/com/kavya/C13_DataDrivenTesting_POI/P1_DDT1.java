7a2058424bd5e57f036e622bfa1c1fef3d4a60b8
KavyaVuragayala <vura.kavya18@gmail.com>
2025-12-08 16:07:30 +0530

    Dec 8

--- src/test/java/com/kavya/C13_DataDrivenTesting_POI/P1_DDT1.java
+++ src/test/java/com/kavya/C13_DataDrivenTesting_POI/P1_DDT1.java
@@ -5,22 +5,31 @@ import org.testng.annotations.Test;
 
 public class P1_DDT1 {
 
-    @Test(dataProvider = "getData")
-    public static void ddt(String username,String password,String ER){
 
-            System.out.println("Running  ");
-            System.out.println(username + " - " + password +" - "  +ER);
-            System.out.println();
-        }
+   @Test(dataProvider = "getData")
+    public static void test_vwo(String username,String password){
 
-        //
-        @DataProvider
-        public Object[][] getData(){
-            return new Object[][]{
-                    new Object[]{"admin@admin.com","pwd123","Expected Result 1"},
-                    new Object[]{"admin@admin.com","pwd123","Expected Result 2"},
-                    new Object[]{"admin@admin.com","pwd123","Expected Result 3"}
-            };
-        }
+       System.out.println("Running");
+       System.out.println(username + "-"+ password);
+
+   }
+
+
+     // excel - 2D file with rows and columns
+    // create a function with 2D
+    // this function is providing data so testng should know
+
+    @DataProvider
+    public Object[][] getData(){
+       //dummy data
+        return new Object[][]{
+                new Object[]{"admin@admin.com","password123"},
+                new Object[]{"admin@admin.com","password123"},
+                new Object[]{"admin@admin.com","password123"},
+                new Object[]{"admin@admin.com","password123"}
+
+        };
     }
+
+}
 
